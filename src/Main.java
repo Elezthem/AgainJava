5bcfcfb71e47157e07c6293f0fadb50defbb83ba
ElezthemDev <97851451+Elezthem@users.noreply.github.com>
2023-10-29 08:37:40 +0200

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 10;
+        String x = "HI";
+        String name = " Alex";
+        System.out.println(x+name);
+        System.out.println("Hello"+ " "+ "Sergey");
+        System.out.println("My numbers is "+ a);
+    }
+}
